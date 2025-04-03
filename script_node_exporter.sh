8b699fbb040fe95f586d76b5a6776edea8483700
NHDuong0912 <66430793+NHDuong0912@users.noreply.github.com>
2025-04-04 04:35:48 +0700

    Update script_node_exporter.sh

--- script_node_exporter.sh
+++ script_node_exporter.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
+set -e  # Dừng script nếu có lỗi xảy ra
+
+# Hàm xử lý lỗi và in thông báo lỗi cụ thể
+trap 'echo "Lỗi xảy ra: $?"' ERR
 
 # Kiểm tra nếu không có tham số
-if [ -z "$1" ]; then
-  echo "Vui lòng cung cấp port. Ví dụ: ./file.sh 9000"
+if [ $# -ne 1 ]; then
+  echo "Sử dụng: ./script_node_exporter.sh <PORT>"
   exit 1
 fi
 
 PORT=$1
 
-# Chạy container Docker với port do người dùng nhập vào
+# Kiểm tra nếu container với tên node-exporter đã tồn tại
+if docker ps -a --format '{{.Names}}' | grep -q "^node-exporter$"; then
+  echo "Container node-exporter đã tồn tại. Dừng và xóa container cũ."
+  docker rm -f node-exporter
+fi
+
+# Chạy Docker container với Node Exporter
 docker run -d --name=node-exporter \
   -p $PORT:9100 \
   --restart unless-stopped \
